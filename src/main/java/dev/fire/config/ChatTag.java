450f37b60e64502b73324bf3c891aa3eb0e27c07
firef1a <hayden@willowmail.com>
2024-06-02 19:40:55 -0700

    added minimessage integration

--- src/main/java/dev/fire/config/ChatTag.java
+++ src/main/java/dev/fire/config/ChatTag.java
@@ -2,7 +2,9 @@ package dev.fire.config;
 
 import com.google.gson.JsonObject;
 import net.minecraft.network.message.SentMessage;
+import net.minecraft.text.Text;
 
+import java.awt.*;
 import java.io.Serializable;
 import java.util.Map;
 
@@ -31,6 +33,55 @@ public class ChatTag implements Serializable {
         object.addProperty("TextContent", TextContent);
         object.addProperty("addBoldSpace", addBoldSpace);
         return object.toString();
+    }
+
+    public String toLeftMiniMessage() {
+        String minimessage = "";
+        minimessage = minimessage + convertStringWithColorToMiniMessage("[", BracketColor);
+        minimessage = minimessage + convertStringWithColorToMiniMessage(Symbol, SymbolColor);
+        if ( addBoldSpace ) minimessage = minimessage + "<bold>\u200C</bold>";
+        return minimessage;
+    }
+    public String toRightMiniMessage() {
+        String minimessage = "";
+        if ( addBoldSpace ) minimessage = minimessage + "<bold>\u200C</bold>";
+        minimessage = minimessage + convertStringWithColorToMiniMessage(Symbol, SymbolColor);
+        minimessage = minimessage + convertStringWithColorToMiniMessage("]", BracketColor);
+        return minimessage;
+    }
+
+    public String toMainMiniMessage() {
+        String minimessage = "";
+        minimessage = minimessage + convertStringWithColorToMiniMessage(TextContent, TextColor);
+
+        return minimessage;
+    }
+
+    public String toShortValue() {
+        String minimessage = "";
+        String subString = TextContent.substring(0,1);
+
+        minimessage = minimessage + convertStringWithColorToMiniMessage("[", BracketColor);
+        minimessage = minimessage + convertStringWithColorToMiniMessage(subString, TextColor);
+        minimessage = minimessage + convertStringWithColorToMiniMessage("]", BracketColor);
+
+        return minimessage;
+    }
+
+    public MiniMessageChatTag toMiniMessageClass() {
+        String minimessage = "";
+
+        return new MiniMessageChatTag(this);
+    }
+
+    private String convertStringWithColorToMiniMessage(String string, int color){
+        Color c = new Color(color);
+        int r = c.getRed();
+        int g = c.getGreen();
+        int b = c.getBlue();
+        String hex = String.format("#%02x%02x%02x", r, g, b);
+        String minimessage = "<" + hex + ">" + string + "</" + hex + ">";
+        return minimessage;
     }
 
 }
